a1625fb1a7ddab18ac4455629e6ebe51c17b60ae
saphal-s <subedisaphal0992gmail.com>
2023-07-15 10:03:01 +0545

    first commit

---
+++ CountDownTimer.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import Clock from './Clock';
+import './cstyle.css'
+
+
+const CountDownTimer = () => {
+
+    const [timerDays, setTimerDays] = useState();
+    const [timerHours, setTimerHours] = useState();
+    const [timerMinutes, setTimerMinutes] = useState();
+    const [timerSeconds, setTimerSeconds] = useState();
+
+
+    const startTimer = () => {
+
+        const countDownDate = new Date("July 30,2022 ").getTime();
+
+        let interval = setInterval(() => {
+
+            const now = new Date().getTime();
+
+            const distance = countDownDate - now;
+
+            const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+            const hours = Math.floor(
+                (distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+            );
+            const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+            const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+
+            if (distance < 0) {
+                // Stop Timer
+
+                clearInterval(interval.current);
+            } else {
+                // Update Timer
+                setTimerDays(days);
+                setTimerHours(hours);
+                setTimerMinutes(minutes);
+                setTimerSeconds(seconds);
+            }
+        });
+    };
+
+    useEffect(() => {
+        startTimer();
+    });
+
+    return (
+        <center>
+            <div className='clock'>
+                <Clock
+                    timerDays={timerDays}
+                    timerHours={timerHours}
+                    timerMinutes={timerMinutes}
+                    timerSeconds={timerSeconds}
+                />
+            </div>
+        </center>
+    )
+}
+
+export default CountDownTimer
